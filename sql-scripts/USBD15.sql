fb0db011e570b8ce16a36f757031b63b096b028d
Bruno Pinto <1250652@isep.ipp.pt>
2025-10-23 14:54:43 +0100

    Update USBD15

--- sql-scripts/USBD15.sql
+++ sql-scripts/USBD15.sql
@@ -2,4 +2,4 @@ SELECT COUNT(l.id) AS "Number of Lines Depart"
 FROM Line l
          JOIN Station s
               ON l.startStation = s.idStation
-WHERE s.nameStation = '&name_station';
+WHERE s.nameStation = 'Nine';
