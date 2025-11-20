b720d9d94ee82351b346ec5826f5323b7745f704
Bruno Pinto <1250652@isep.ipp.pt>
2025-11-20 11:18:11 +0000

    Update USAC07, USAC08 and USAC09

--- ARQCP/USAC07/main.c
+++ ARQCP/USAC07/main.c
@@ -1,8 +1,97 @@
-#include <stdio.h>
+#include <string.h>
+#include "../../unity.h"
 #include "move_n_to_array.h"
 
-int main(void) {
 
+int callfunc ( int (*f)(int* array, int len, int * n_elem, int* rd, int* wr, int num, int* vec ),
+                          int* array, int len, int * n_elem, int* rd, int *wr, int num, int* vec );
 
-    return 0;
+void setUp(void) {
+    // set stuff up here
 }
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+
+
+void run_test(int * array, int len, int n_elem, int rd, int wr, int num,
+			  int exp_res, int exp_elem, int * exp_vec, int exp_rd, int exp_wr)
+{
+    int v1[100];
+	// 0 - sentinel
+	// 1 - read
+	// 2 - sentinel
+	// 3 - write
+    // 4 - sentinel
+	// 5 - n_elem
+	// 6 - sentinel
+	// 7 - buffer
+	// 7+length - sentinel
+
+    // setup
+    memset(v1, 0x55, sizeof v1);
+	memcpy(v1+7,array,len*sizeof(int));  // buffer
+	v1[1]=rd;
+	v1[3]=wr;
+	v1[5]=n_elem;
+
+	int v2[100];
+    memset(v2, 0x55, sizeof v2);    // destination
+    int res;
+	res=callfunc(move_n_to_array, v1+7, len, v1+5, v1+1, v1+3, num, v2+1);
+    TEST_ASSERT_EQUAL_INT(exp_res,res);    // check result
+    TEST_ASSERT_EQUAL_INT(0x55555555, v1[0]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, v1[2]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, v1[4]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, v1[6]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, v1[len+7]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT_ARRAY(array, v1+7, len);    // check buffer
+    TEST_ASSERT_EQUAL_INT(exp_rd, v1[1]);    // check read
+    TEST_ASSERT_EQUAL_INT(exp_wr, v1[3]);    // check write
+    TEST_ASSERT_EQUAL_INT(exp_elem, v1[5]);    // check num_elem
+    // output vector
+    TEST_ASSERT_EQUAL_INT(0x55555555, v2[0]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, v2[num+1]);    // check sentinel
+    if (exp_res==1)
+    	TEST_ASSERT_EQUAL_INT_ARRAY(exp_vec,v2+1, num);
+		// check output
+	else
+    	TEST_ASSERT_EQUAL_INT(0x55555555, v2[1]);    // check output
+}
+// void run_test(int * array, int len,int elem, int rd, int wr, int num,
+//			  int exp_res, int exp_elem,  int * exp_vec, int exp_rd, int exp_wr)
+
+void test_One()
+{
+    run_test((int[]){0,0,0}, 3, 0, 0, 0, 1,
+							0, 0, (int[]){0,0,0}, 0, 0);
+}
+void test_Zero()
+{
+    run_test((int[]){1,0,0}, 3, 1, 0, 1, 1,
+							1, 0, (int[]){1}, 1, 1);
+}
+void test_Three()
+{
+    run_test((int[]){1,2,3,4}, 4, 3, 3, 2, 3,
+					1, 0, (int[]){4,1,2}, 2, 2);
+}
+void test_Five()
+{
+    run_test((int[]){1,2,3,4}, 4, 2, 2, 1, 3,
+					0, 2, (int[]){3,4,1}, 2, 1);
+}
+
+int main()
+  {
+
+    UNITY_BEGIN();
+    RUN_TEST(test_One);
+    RUN_TEST(test_Zero);
+    RUN_TEST(test_Three);
+    RUN_TEST(test_Five);
+    return UNITY_END();
+
+  }
--- ARQCP/USAC09/main.c
+++ ARQCP/USAC09/main.c
@@ -1,8 +1,88 @@
-#include <stdio.h>
+#include <string.h>
+#include "../../unity.h"
 #include "median.h"
 
-int main(void) {
 
+int callfunc ( int (*f)(int* ptr, int num, int * me),int* ptr, int num, int* me);
 
-    return 0;
+void setUp(void) {
+    // set stuff up here
 }
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+
+
+void run_test(int * vec, int in_num, int exp_res , int  exp_med)
+{
+    int vec1[100];
+    int res;
+
+    int med[3]={-1,-1,-1};
+
+    // setup
+        memset(vec1, 0x55, sizeof vec1);
+
+	memcpy(vec1+1,vec,in_num*sizeof(int));  //
+	res=callfunc(median,vec1+1,in_num,&med[1]);
+
+    TEST_ASSERT_EQUAL_INT(0x55555555, vec1[in_num+1]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(0x55555555, vec1[0]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(exp_res, res);    // check res
+    if (exp_res==1)
+	    TEST_ASSERT_EQUAL_INT(exp_med, med[1]);    // check median
+	else
+	    TEST_ASSERT_EQUAL_INT(-1, med[1]);    // check median
+
+    TEST_ASSERT_EQUAL_INT(-1, med[0]);    // check sentinel
+    TEST_ASSERT_EQUAL_INT(-1, med[2]);    // check sentinel
+
+}
+
+//void run_test(int * vec, int in_num, int exp_res , int  exp_med)
+
+void test_NullVector()
+{
+    run_test((int[]){0},0,0,0);
+}
+void test_One()
+{
+    run_test((int[]){1000},1,1,1000);
+}
+void test_Zero()
+{
+    run_test((int[]){10,0,1},3,1,1);
+}
+void test_Three()
+{
+    run_test((int[]){-1,-3,-2},3,1,-2);
+}
+void test_Four()
+{
+    run_test((int[]){-1,-3,-4,-2},4,1,-3);
+}
+void test_Five()
+{
+    run_test((int[]){1,1,1,1,2},5,1,1);
+}
+void test_Six()
+{
+    run_test((int[]){0,3000,10,20,0,300},6,1,15);
+}
+
+int main()
+  {
+
+    UNITY_BEGIN();
+    RUN_TEST(test_NullVector);
+    RUN_TEST(test_One);
+    RUN_TEST(test_Zero);
+    RUN_TEST(test_Three);
+    RUN_TEST(test_Four);
+    RUN_TEST(test_Five);
+    RUN_TEST(test_Six);
+    return UNITY_END();
+
+  }
